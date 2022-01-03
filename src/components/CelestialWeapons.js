4aa9c98434c5a65e3b512a2aea843d93e5704410
Michael Harris <xero5k@yahoo.com>
2022-01-02 20:25:29 -0600

    Completed assignment

--- src/components/CelestialWeapons.js
+++ src/components/CelestialWeapons.js
@@ -1,6 +1,5 @@
 import React from "react";
 import List from "./List";
-import { Link } from "react-router-dom";
  
 function CelestialWeapons({celestial}){
   console.log("(Ce)cels: ",celestial)
@@ -12,23 +11,12 @@ function CelestialWeapons({celestial}){
     }
   )
 
-  /* const renderCels = Object.keys(celestial).map((celID)=>(
-    <li key={celID}>
-      <Link to={`/characters/${celID}`}>{celestial[celID]}</Link>  
-    </li>,
-    console.log("Links: ", "./characters/name")
-  )); 
-    this was an attempt to link the character
-    with their celestial weapon
-  */
-
   return(
     <div>
       <h1>Welcome to the Celestial Weapons Page!</h1>
       <h5 style={{color: "grey"}}>(These are the Ultimate Weapons in the game)</h5>
       <ul>
         {displayCels} 
-        {/* renderCels */}
       </ul>
     </div>
   )
